e3c247f6fab421250bc9ffaa6aa28cf7ad7d2bc8
vse <D:\mail\The Bat!>
2018-07-06 16:47:58 +0300

    login form

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	header('Location: view/app/login.php');
+	// session_start();
+	// if ($_SESSION['login_user'] != NULL && $_SESSION['login_user'] != "")
+ //    	header('Location: cams.php');
+	// else
+ //    	header('Location: config/setup.php');
+?>
---
+++ view/app/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../style/login.css">
+	<meta charset="utf-8">
+	<title>Добро пожаловать | ВМатча</title>
+</head>
+<body>
+	<div class="login_header">
+		<div class="container">
+			<div class="row" id="page-login-header">
+				<div class="col-12">
+					<h3 id="main-name-page-login">VM</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				<h3>Hello!</h3>
+			</div>
+			<div class="col-4">
+				<div id="block-login-page-login">
+					<form id="form-login-page-login">
+						<input type="email" name="email" placeholder="Введите Email...">
+						<input type="password" name="password" placeholder="Введите пароль...">
+						<input type="submit" name="come-in" value="Войти">
+						<div>
+							<a href="#">Забыли пароль?</a>
+						</div>
+					</form>
+				</div>
+				<div id="form-register-page-login">
+					<input type="text" name="first_name" placeholder="Ваше имя">
+					<input type="text" name="second_name" placeholder="Ваша фамилия">
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
